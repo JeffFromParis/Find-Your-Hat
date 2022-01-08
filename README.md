5b15987432902205868f9d7e9bc57c22d0b5c810
Jeff Vergne <vergnejf@gmail.com>
2022-01-08 14:50:43 +0100

    Update README and create the field class including print method

---
+++ README.md
@@ -0,0 +1,14 @@
+#Find Your Hat
+*A Project by Jeff Vergne*
+
+This project is part of the **Fullstack Engineer Career** path from CodeCademy
+
+## Objective
+This project is about building an interactive terminal game with JavaScript classes.
+
+### Subject
+The purpose for the player is to find its hat without falling in one of the numerous holes existing on its way.
+This is a step by step game, where at each step, the game will ask the player which direction he/she wants to go.
+
+## Technology
+Javascript
--- main.js
+++ main.js
@@ -5,4 +5,47 @@ const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
 
-console.log('Hey there');
+class Field {
+// field is a bidimensionnal array
+    constructor(field){
+      this._field = field;
+    }
+
+    //GETTERS & SETTERS
+    get field() {
+        return this._field;
+    }
+
+    //METHODS
+
+    //print will dsiplay the field on the temrinal, turning the doubel array into several strings
+    print(){
+        for (var row = 0; row < this.field.length; row++){
+            console.log(this.field[row].join('')); //join allows the transformation from an array to a string. the parameter will be the separator between the different elements of the array.
+        }
+    }
+}
+
+/**************************************************************
+ * GAME
+ */
+const name = prompt('Welcome to "Find Your Hat"! What is your name? ');
+console.log(`Hey there ${name}. Here is your playground. Will you manage to find your hat?`);
+myField.print();
+let direction = prompt('Which direction should you go next?');
+
+
+/**************************************************************
+ * TESTING ZONE
+ */
+const myField = new Field([
+    ['*', '░', 'O'],
+    ['░', 'O', '░'],
+    ['░', '^', '░'],
+]);
+
+console.log(myField.field);
+myField.print();
+
+
+
